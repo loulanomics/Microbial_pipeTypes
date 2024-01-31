4a13a388a33dddb1b00c358cb8147b4b10d50839
Lou LaMartina <48258356+loulanomics@users.noreply.github.com>
2024-01-31 12:54:08 -0600

    Update 03_PipeType_diversity.R

--- 03_PipeType_diversity.R
+++ 03_PipeType_diversity.R
@@ -129,6 +129,146 @@ mini2.plot <-
 ggsave("PLOTS/mini2.pdf", plot = mini2.plot, device = "pdf", width = 2, height = 2, units = "in")
 
 
+# ranges
+signif(range(subset(pcoa_data, Site_city == "Milwaukee")$Axis.1), 2) # -0.56  0.28
+signif(range(subset(pcoa_data, Site_city == "Milwaukee")$Axis.2), 2) # -0.23  0.29
+
+signif(range(subset(pcoa_data, Site_city == "Waukesha")$Axis.1), 2) # 0.13 0.21
+signif(range(subset(pcoa_data, Site_city == "Waukesha")$Axis.2), 2) # 0.13 0.22
+
+signif(range(subset(pcoa_data, Site_city == "Oak Creek" & Sample_ID != "OC_1_HS_H")$Axis.1), 2) # 0.066 0.096
+signif(range(subset(pcoa_data, Site_city == "Oak Creek" & Sample_ID != "OC_1_HS_H")$Axis.2), 2) # -0.23 -0.18
+signif(subset(pcoa_data, Sample_ID == "OC_1_HS_H")[c("Axis.1", "Axis.2")], 2) # -0.18   0.27
+
+
+
+
+#################
+### distances ###
+#################
+
+# extract distances
+dist_data <- data.frame(Sample_ID_x = rownames(relabun), scores(bray.stat))
+
+
+# get comparisons
+dist_data <- melt(dist_data, variable.name = "Sample_ID_y", value.name = "Bray", id.vars = "Sample_ID_x")
+dist_data$Sample_ID_y <- as.character(dist_data$Sample_ID_y)
+dist_data$Compare <- NA
+for (i in 1:nrow(dist_data)) {
+  dist_data$Compare[i] <- paste(sort(unlist(dist_data[i, c("Sample_ID_x", "Sample_ID_y")])), collapse = "__")
+}
+dist_data <- unique(dist_data[dist_data$Bray > 0, c("Compare", "Bray")])
+
+
+# add info - x
+dist_data$Sample_ID <- sapply(strsplit(dist_data$Compare, "__"), '[', 1)
+dist_data <- merge(dist_data, info[c("Sample_ID", "Sample_name", "Site_city", "Pipe_material", "Pipe_ID", "Sample_surface")], by = "Sample_ID")
+colnames(dist_data)[colnames(dist_data) %in% colnames(info)] <- 
+  paste0(colnames(dist_data)[colnames(dist_data) %in% colnames(info)], "_x")
+
+
+# add info - y
+dist_data$Sample_ID <- sapply(strsplit(dist_data$Compare, "__"), '[', 2)
+dist_data <- merge(dist_data, info[c("Sample_ID", "Sample_name", "Site_city", "Pipe_material", "Pipe_ID", "Sample_surface")], by = "Sample_ID")
+colnames(dist_data)[colnames(dist_data) %in% colnames(info)] <- 
+  paste0(colnames(dist_data)[colnames(dist_data) %in% colnames(info)], "_y")
+write.csv(dist_data, "DATA/BrayCurtis_distances.csv", row.names = F, na = "", quote = F)
+
+
+
+
+########################
+### median abs diffs ###
+########################
+
+# within city
+dist_city <- subset(dist_data, Site_city_x == Site_city_y)
+dist_city$Within <- "1 city"
+nrow(dist_city) # 1296
+
+
+# within surface
+dist_surface <- subset(dist_data, Sample_surface_x == Sample_surface_y)
+dist_surface$Within <- "2 surface"
+nrow(dist_surface) # 1103
+
+
+# within type
+dist_type <- subset(dist_data, Pipe_material_x == Pipe_material_y)
+dist_type$Within <- "3 type"
+nrow(dist_type) # 811
+
+
+# type + city
+dist_city_type <- subset(dist_data, Site_city_x == Site_city_y & Pipe_material_x == Pipe_material_y)
+dist_city_type$Within <- "4 city+type"
+nrow(dist_city_type) # 451
+dist_city_type$Var <- paste(dist_city_type$Site_city_x, dist_city_type$Pipe_material_x)
+
+
+# type + surface
+dist_surface_type <- subset(dist_data, Sample_surface_x == Sample_surface_y & Pipe_material_x == Pipe_material_y)
+dist_surface_type$Within <- "5 surface+type"
+nrow(dist_surface_type) # 355
+dist_surface_type$Var <- paste(dist_surface_type$Sample_surface_x, dist_surface_type$Pipe_material_x)
+
+
+# within pipe
+dist_pipe <- subset(dist_data, Pipe_ID_x == Pipe_ID_y)
+dist_pipe$Within <- "6 pipe"
+nrow(dist_pipe) # 153
+
+
+# within sample
+dist_sample <- subset(dist_data, Sample_name_x == Sample_name_y)
+dist_sample$Within <- "7 sample"
+nrow(dist_sample) # 53
+
+
+# median absolute differences
+mads <- rbind(
+  data.frame(Test = "1 City",
+             Variable = aggregate(Bray ~ Site_city_x, mad, data = dist_city[c("Bray", "Site_city_x")])[,1],
+             MAD = aggregate(Bray ~ Site_city_x, mad, data = dist_city[c("Bray", "Site_city_x")])[,2],
+             n = data.frame(table(info$Site_city))$Freq),
+  data.frame(Test = "2 Surface",
+             Variable = aggregate(Bray ~ Sample_surface_x, mad, data = dist_surface[c("Bray", "Sample_surface_x")])[,1],
+             MAD = aggregate(Bray ~ Sample_surface_x, mad, data = dist_surface[c("Bray", "Sample_surface_x")])[,2],
+             n = data.frame(table(info$Sample_surface))$Freq),
+  data.frame(Test = "3 Type",
+           Variable = aggregate(Bray ~ Pipe_material_x, mad, data = dist_type[c("Bray", "Pipe_material_x")])[,1],
+             MAD = aggregate(Bray ~ Pipe_material_x, mad, data = dist_type[c("Bray", "Pipe_material_x")])[,2],
+             n = data.frame(table(info$Pipe_material))$Freq),
+  data.frame(Test = "4 City+Type",
+             Variable = aggregate(Bray ~ Var, mad, data = dist_city_type[c("Bray", "Var")])[,1],
+             MAD = aggregate(Bray ~ Var, mad, data = dist_city_type[c("Bray", "Var")])[,2],
+             n = subset(data.frame(table(paste(info$Site_city, info$Pipe_material))), Freq > 1)$Freq),
+  data.frame(Test = "5 Surface+Type",
+             Variable = aggregate(Bray ~ Var, mad, data = dist_surface_type[c("Bray", "Var")])[,1],
+             MAD = aggregate(Bray ~ Var, mad, data = dist_surface_type[c("Bray", "Var")])[,2],
+             n = subset(data.frame(table(paste(info$Sample_surface, info$Pipe_material))), Freq > 1)$Freq))
+write.csv(mads, "DATA/MAD_scores.csv", row.names = F, na = "", quote = F)
+
+
+# significantly different variances?
+signif(leveneTest(Bray ~ as.factor(Site_city_x), data = dist_city)$`Pr(>F)`[1], 3)         # 3.44e-32
+signif(leveneTest(Bray ~ as.factor(Sample_surface_x), data = dist_surface)$`Pr(>F)`[1], 3) # 0.0212
+signif(leveneTest(Bray ~ as.factor(Pipe_material_x), data = dist_type)$`Pr(>F)`[1], 3)     # 8.94e-17
+signif(leveneTest(Bray ~ as.factor(Var), data = dist_city_type)$`Pr(>F)`[1], 3)            # 1.48e-17
+signif(leveneTest(Bray ~ as.factor(Var), data = dist_surface_type)$`Pr(>F)`[1], 3).        # 8.37e-11
+
+
+# plot
+mads.plot <-
+  ggplot(mads, aes(x = Variable, y = MAD, fill = Test)) +
+  geom_col() +
+  geom_text(data = mads, aes(x = Variable, y = 0, label = n), nudge_y = -0.01, size = 2) +
+  facet_grid(~ Test, scales = "free", space = "free") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave("PLOTS/mads.pdf", plot = mads.plot, device = "pdf", width = 9, height = 6, units = "in")
+
+
 
 
 #################
@@ -137,8 +277,7 @@ ggsave("PLOTS/mini2.pdf", plot = mini2.plot, device = "pdf", width = 2, height =
 
 # permanova
 identical(rownames(relabun), info$Sample_ID)
-perman <- adonis2(bray.stat ~ Pipe_line * Pipe_material * Pipe_age_years * 
-                    Pipe_diameter_in * Sample_surface * Sample_local, 
+perman <- adonis2(bray.stat ~ Site_city + Pipe_material + Sample_surface,
                   data = info, na.action = na.exclude)
 
 
@@ -148,5 +287,38 @@ colnames(perman)[6] <- "p"
 perman$R2 <- signif(perman$R2, 3)
 
 
-# save
-write.csv(perman, "DATA/permanova.csv", row.names = F, na = "", quote = F)
+# post-hoc pairwise multilevel comparison
+posthoc <- pairwise.adonis2(relabun ~ Site_city + Pipe_material + Sample_surface, 
+                            data = info)
+posthoc <- do.call(rbind, posthoc[-1])
+posthoc <- data.frame(Comparison = sapply(strsplit(rownames(posthoc), "\\."), '[', 1),
+                      Variable = sapply(strsplit(rownames(posthoc), "\\."), '[', 2),
+                      posthoc)
+
+
+# keep p values
+posthoc$Pr..F. <- signif(posthoc$Pr..F., 3)
+posthoc.p <- dcast(Variable ~ Comparison, value.var = "Pr..F.", data = posthoc)
+colnames(posthoc.p)[-1] <- paste0(colnames(posthoc.p)[-1], ".p")
+
+
+# keep R squared values
+posthoc$R2 <- signif(posthoc$R2, 3)
+posthoc.R2 <- dcast(Variable ~ Comparison, value.var = "R2", data = posthoc)
+colnames(posthoc.R2)[-1] <- paste0(colnames(posthoc.p)[-1], ".R2")
+
+
+# combine p and R2
+posthoc.p <- merge(posthoc.p, posthoc.R2, by = "Variable")
+posthoc.p <- posthoc.p[c("Variable", sort(colnames(posthoc.p)[-1]))]
+
+
+# combine permanova
+perman <- merge(perman, posthoc.p, by = "Variable")
+write.csv(perman, "DATA/PipeType_permanova.csv", row.names = F, na = "", quote = F)
+
+
+# confidence intervals on the differences between the mean distance-to-centroid
+plot(TukeyHSD(betadisper(bray.stat, info$Site_city)))
+plot(TukeyHSD(betadisper(bray.stat, info$Pipe_material)))
+plot(TukeyHSD(betadisper(bray.stat, info$Sample_surface)))
